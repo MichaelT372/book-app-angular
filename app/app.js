a7280e92628a92987c124380c102fa5321af1293
Michael <michaelthomas372@gmail.com>
2015-12-02 15:23:42 +0100

    implemented main page pagination and filtering

--- app/app.js
+++ app/app.js
@@ -1,15 +1,16 @@
 (function (){
-    var app = angular.module('reedsyApp', ['ngRoute', 'ngAnimate']);
-    app.config(function ($routeProvider){
+    var app = angular.module('reedsyApp', ['ngRoute', 'ngAnimate', 'ui.bootstrap']);
+    app.config(function ($routeProvider, $locationProvider){
         $routeProvider
             .when('/', {
-                controller: 'CustomersController',
+                controller: 'booksController',
                 templateUrl: 'app/views/books.html'
             })
             .when('/books/:bookId', {
-                controller: 'OrdersController',
+                controller: 'singleBookController',
                 templateUrl: 'app/views/single-book.html'
             })
             .otherwise({ redirectTo: '/' });
+        $locationProvider.html5Mode(true);
     });
 }());
---
+++ app/controllers/booksController.js
@@ -0,0 +1,50 @@
+(function() {
+
+    var booksController = function($scope, filterFilter, booksFactory) {
+        //loading data from factory
+        $scope.books = booksFactory.books;
+        $scope.genres = booksFactory.genres;
+
+        // pagination controls
+	    $scope.currentPage = 1;
+	    $scope.totalItems = $scope.books.length;
+	    $scope.itemsPerPage = 4; //items displayed per page
+	    $scope.pages = 5; //amount of pages to show in pagination bar
+
+        $scope.$watch('search', function (newVal, oldVal) {
+            $scope.filtered = filterFilter($scope.books, newVal);
+            $scope.totalItems = $scope.filtered.length;
+            $scope.noOfPages = Math.ceil($scope.totalItems / $scope.itemsPerPage);
+            $scope.currentPage = 1;
+        }, true);
+
+        $scope.$watch('genre', function (newVal, oldVal) {
+            $scope.filtered = filterFilter($scope.books, newVal);
+            $scope.totalItems = $scope.filtered.length;
+            $scope.noOfPages = Math.ceil($scope.totalItems / $scope.itemsPerPage);
+            $scope.currentPage = 1;
+        }, true);
+
+        $scope.timeSince = function(date) {
+            return booksFactory.timeSince(date);
+        }
+    };
+
+    //injects dependencies to avoid errors with minification
+    booksController.$inject = ['$scope', 'filterFilter', 'booksFactory',];
+
+    angular.module('reedsyApp')
+        .filter('startFrom', function () {
+            return function (input, start) {
+                if (input) {
+                    start = +start;
+                    return input.slice(start);
+                }
+                return [];
+            };
+        });
+
+    angular.module('reedsyApp')
+        .controller('booksController', booksController);
+
+}());
